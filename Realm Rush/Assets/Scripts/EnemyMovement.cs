abd0455312606deee3b2235e30c0bba414577ba1
rahulpargi641 <61396833+rahulpargi641@users.noreply.github.com>
2020-10-01 11:15:01 +0530

    added shortest pathfinding ,assest

---
+++ Realm Rush/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float movementPeriod = .5f;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+
+        Pathfinder pathfinder = FindObjectOfType<Pathfinder>();
+        var path = pathfinder.GetPath();
+        StartCoroutine(FollowPath(path));
+    }
+
+    IEnumerator FollowPath(List<Waypoint> path)
+    {
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(movementPeriod);
+        }
+       
+    }
+
+}
